c92fe0a6869e16683f05a2d374c3f81d8fcde7cb
ehoang0001 <ehoang0001@mymail.lausd.net>
2024-10-01 09:12:50 -0700

    Update Neighborhoodrunner.java

--- Neighborhoodrunner.java
+++ Neighborhoodrunner.java
@@ -3,12 +3,12 @@ import org.code.neighborhood.*;
 public class NeighborhoodRunner {
   public static void main(String[] args) {
 
-    MuralPainter kai = new MuralPainter();
+    MuralPainter ethan = new MuralPainter();
     PyramidPainter tut = new PyramidPainter();
      CloudPainter suns = new CloudPainter();
          CloudPainter UFOs = new CloudPainter();
 
-kai.paintBackground("blue", 32);
+ethan.paintBackground("blue", 32);
 tut.paintPyramid();
 suns.sun();
 UFOs.UFO();
